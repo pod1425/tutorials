b2dd5f3eed3db61794cd18d683878f612697cd68
Streaker1425 <i.o.tkachov@student.khai.edu>
2023-03-07 19:01:58 +0200

    lesson 1-2

---
+++ src/lesson1/Hello.java
@@ -0,0 +1,9 @@
+package lesson1;
+
+public class Hello {
+    public static void main(String[] args) {
+        String text = "Hello world";
+
+        System.out.println(text);
+    }
+}
---
+++ src/lesson2/Operators.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int num = 50;
+        int number = 10 + --num;
+        System.out.println(number);
+        System.out.println(num);
+    }
+}
