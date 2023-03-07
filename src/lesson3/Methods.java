8e3b859f287646c0c7b5fe086892fbc12e36dd76
Streaker1425 <i.o.tkachov@student.khai.edu>
2023-03-07 19:51:22 +0200

    lesson 3

---
+++ src/lesson3/Methods.java
@@ -0,0 +1,39 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class Methods {
+
+    public static void print(String text) {
+        System.out.println(text);
+    }
+
+    /**
+     * Adds two numbers!
+     * @param a first number
+     * @param b second number
+     * @return sum of those two numbers
+     */
+    public static int sum(int a, int b) {
+        int lol = 1 / 0;
+        return a + b;
+    }
+
+    public static int askNumber(String message) {
+        print(message);
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static void run() {
+        String greeting = "hello! I can add two numbers!";
+        print(greeting);
+        int a = askNumber("Input first number>");
+        int b = askNumber("Input second number>");
+        print("Result:");
+        System.out.println(sum(a, b));
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
