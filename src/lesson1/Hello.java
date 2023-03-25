95875d38fd81ba5d57822e2f305e41cd455b9578
Streaker1425 <i.o.tkachov@student.khai.edu>
2023-03-25 15:55:25 +0200

    Lesson 1-2

---
+++ src/lesson1/Hello.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ src/lesson2/Types.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class Types {
+    public static void main(String[] args) {
+        String text = "Hello!";
+        int number = 5;
+        String toOutput = text + " Here, have a number: " + number;
+        System.out.println(toOutput);
+    }
+}
